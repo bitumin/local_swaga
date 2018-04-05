4e7777524189dfbf46b882f0eb3ba85f4729f80e
Mitxel <mitxel@tresipunt.com>
2018-04-05 15:52:29 +0200

    set min moodle version to 2.7

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_swaga';
 $plugin->version = 2018040500;
 $plugin->release = 'v0.0';
-$plugin->requires = 2017051500;
+$plugin->requires = 2014051200; // Moodle >= 2.7
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->cron = 0;
 $plugin->dependencies = array();
